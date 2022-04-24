9adac9e46e4aecfe19e26ee734bb72f1c486cc74
Stephane Paquet <spaquet@up4b.com>
2022-04-23 17:18:39 -0700

    Adding the Game schema, model and controller

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,10 @@ class Game < ApplicationRecord
   belongs_to :user, class_name: "user", foreign_key: "user_id"
 
   has_one_attached :image
+  
+  validates :name,  presence: true, length: { maximum: 250 }
+  validates :limit, presence: true, numericality: { other_than: 0 }
+  validates :image, content_type: [:png, :jpg, :jpeg], size: { between: 1.kilobyte..10.megabytes , message: 'is not given between size' }
+  
+  enum :status, [ draft: 0, published: 10, archived: 20, trashed: 50 ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_22_183946) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_23_234629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,50 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_22_183946) do
     t.string "remember_token", null: false
     t.index ["remember_token"], name: "index_active_sessions_on_remember_token", unique: true
     t.index ["user_id"], name: "index_active_sessions_on_user_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "user_id", null: false
+    t.boolean "shareable", default: false, null: false
+    t.boolean "public", default: false, null: false
+    t.integer "limit", default: 20, null: false
+    t.integer "status_id", default: 0, null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["public"], name: "index_games_on_public"
+    t.index ["shareable"], name: "index_games_on_shareable"
+    t.index ["status_id"], name: "index_games_on_status_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -56,5 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_22_183946) do
   end
 
   add_foreign_key "active_sessions", "users", on_delete: :cascade
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "users", "organizations"
 end
