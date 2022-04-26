8a49c924266cb272bb547f0f1debb2269edabd41
Stephane Paquet <spaquet@up4b.com>
2022-04-26 11:25:11 -0700

    Game status can now be controlled from the creation screen

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,8 +10,13 @@ class GamesController < ApplicationController
   end
 
   def create
-    flash[:success] = "Game created!"
-    redirect_to games_path
+    @game = Game.new(game_params)
+    @game.user_id = current_user.id
+    if @game.save
+      redirect_to games_path, notice: "Game created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -23,11 +28,32 @@ class GamesController < ApplicationController
   end
 
   def update
-
+    if current_user.id == @game.user_id
+      if @game.update(game_params)
+        redirect_to games_path, notice: "Game updated successfully!"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    else
+      flash.now[:alert] = "You are not authorized to edit this game."
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    if current_user.id == @game.user_id
+      Game.find(params[:id]).destroy
+    else
+      flash.now[:alert] = "You are not authorized to delete this game."
+      # render :edit, status: :unprocessable_entity
+      redirect_back(fallback_location: games_path)
+    end
+  end
 
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :description, :user_id, :shareable, :public, :status)
   end
 
 end
