008e9f6f14d1828d6112bdcd77775575bea53fce
Stephane Paquet <spaquet@up4b.com>
2022-05-11 16:29:05 -0700

    Settings scaffold & database migration improvments

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,14 @@
 class Quiz < ApplicationRecord
+  belongs_to :user
+  has_many   :questions, dependent: :destroy
+
+  has_one_attached :image
+  has_rich_text    :description
+
+  enum status: { draft: 0, published: 10, archived: 20, flag_for_deletion: 30 }
+
+  validates :title, presence: true
+  validates :image, content_type: ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'], 
+                    size: { between: 1.kilobyte..5.megabytes , message: 'is not given between size' }
+
 end
---
+++ app/views/settings/_setting.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! setting, :id, :theme, :lang, :created_at, :updated_at
+json.url setting_url(setting, format: :json)
